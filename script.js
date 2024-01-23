1d88bb6549643a3003c3b8c1490b99f7cbd67dc1
Dharsana0431 <sanacs0431@gmail.com>
2024-01-23 13:02:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("para")[0].innerHTML = "Welcome to js";
+document.getElementsByClassName("para")[1].innerHTML= "hello";
+document.getElementById("p1").innerHTML = "hi";
